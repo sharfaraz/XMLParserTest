8ec2433d42ba0dd4af806a0111df091647d106b9
Peer <sharfarazbaari@gmail.com>
2020-01-29 21:12:53 -0600

    Commit the changes

--- XMLParserTest/src/com/peer/DOMParser.java
+++ XMLParserTest/src/com/peer/DOMParser.java
@@ -12,9 +12,24 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-
-
 public class DOMParser {
+
+	private NodeList getChildren(NodeList nodeList) {
+		NodeList childNodeList = null;
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			Node b = nodeList.item(i);
+			if (b.getNodeType() == Node.ELEMENT_NODE) {
+				Element element = (Element) b;
+				System.out.println(element.getTagName() + "=" + element.getTextContent());
+				childNodeList = element.getChildNodes();
+				return childNodeList;
+			} else {
+				System.out.println("No more elements");
+				System.out.println(b.getTextContent() + "=" + b.getTextContent());
+			}
+		}
+		return childNodeList;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -23,37 +38,19 @@ public class DOMParser {
 			DocumentBuilder builder = factory.newDocumentBuilder();
 			Document doc = builder.parse("2027341478.xml");
 			NodeList BillItemGroupList = doc.getElementsByTagName("BillItemsGroup");
-			for ( int i = 0; i< BillItemGroupList.getLength(); i++){
-				Node b = BillItemGroupList.item(i);
-				if (b.getNodeType() == Node.ELEMENT_NODE) {
-					Element billItemGroup = (Element) b;
-					NodeList billItemGroupChildren = billItemGroup.getChildNodes();
-					for ( int j = 0; j<billItemGroupChildren.getLength(); j++) {
-						Node c = billItemGroupChildren.item(j);
-						if ( c.getNodeType() == Node.ELEMENT_NODE ) {
-							Element name = (Element) c;
-							if( name.hasChildNodes()) {
-								System.out.println("has a child node");
-								System.out.println(name.getTagName() + "=" + name.getTextContent());
-							} else {
-								System.out.println(name.getTagName() + "=" + name.getTextContent());
-							}
-						}
-					}
-				}
-			}
-		}
-		catch (ParserConfigurationException e) {
-			e.printStackTrace();			
-		}
-		catch (SAXException e) {
+			DOMParser domParser = new DOMParser();
+			NodeList billItemGroupChildren = domParser.getChildren(BillItemGroupList);
+			NodeList billItemGroupChildren2 = domParser.getChildren(billItemGroupChildren);
+			
+
+		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
-		}
-		catch (IOException e) {
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
 	}
-	
 
 }
